78b33e54272952b1ea58fb2d7d51c012fdc5e2aa
AniaKuchcinska <a.a.kuchcinska@gmail.com>
2026-06-23 16:37:16 +0200

    Added and set up next-intl

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,49 @@
+import type { Metadata } from "next";
+import { Montserrat, Jost } from "next/font/google";
+import "../globals.css";
+import {NextIntlClientProvider, hasLocale} from 'next-intl';
+import {notFound} from 'next/navigation';
+import {routing} from "@/i18n/routing";
+
+
+const fontMontserrat = Montserrat({
+  variable: "--font-montserrat",
+  subsets: ["latin"],
+});
+
+const fontJost = Jost({
+  variable: "--font-jost",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Alchemia Dance Studio",
+  description: "Szkoła tańca Alchemia we Wrześni",
+};
+
+export function generateStaticParams(){
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+export default async function RootLayout({
+                                           children,
+                                           params
+                                         }: {
+  children: React.ReactNode;
+  params: Promise<{locale: string}>;
+}) {
+  const {locale} = await params;
+  if (!hasLocale(routing.locales, locale)) {
+    notFound();
+  }
+
+  return (
+      <html lang={locale}>
+      <body>
+      <NextIntlClientProvider>
+        {children}
+      </NextIntlClientProvider>
+      </body>
+      </html>
+  );
+}
